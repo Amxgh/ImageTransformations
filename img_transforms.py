d47c7dc932142e98548e7ca4b4996a0def5aba9a
Amogh <axm0754@mavs.uta.edu>
2025-01-27 03:49:48 -0600

    created random_square_crop

---
+++ img_transforms.py
@@ -0,0 +1,21 @@
+import numpy as np
+import random
+import cv2
+import matplotlib.pyplot as plt
+from numpy.lib import stride_tricks
+
+
+def random_square_crop(image: np.ndarray, size:int):
+    h, w, c = image.shape
+
+    if size > min(w, h) or size < 0:
+        raise ValueError(f"Size is greater than min(w, h) or less than 0. For this image, the dimensions are: {h}x{w}x{c}")
+
+    start_row = random.randint(0, h - size)
+    end_row = start_row + size
+
+    start_col = random.randint(0, w - size)
+    end_col = start_col + size
+
+    return image[start_row:end_row, start_col:end_col, :]
+
