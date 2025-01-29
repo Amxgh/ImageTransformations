12154408676a04c7df0be9cec18a1b27341adb62
Amogh <axm0754@mavs.uta.edu>
2025-01-28 21:58:14 -0600

    Implemented resize_img

--- img_transforms.py
+++ img_transforms.py
@@ -3,6 +3,8 @@ import random
 import cv2
 import matplotlib.pyplot as plt
 from numpy.lib import stride_tricks
+from color_space_test import rgb_to_hsv, hsv_to_rgb
+from random import randint, uniform
 
 
 def random_crop(img: np.ndarray, size: int) -> np.ndarray:
@@ -31,6 +33,26 @@ def extract_patch(img: np.ndarray, num_patches: int) -> np.ndarray:
     return patches
 
 # def resize_img(img: np.ndarray, factor: int) -> np.ndarray:
+def resize_img(img: np.ndarray, factor: int) -> np.ndarray:
+    h, w, c = img.shape
+
+    new_h: int = int(h * factor)
+    new_w: int = int(w * factor)
+
+    resized_img: np.ndarray = np.zeros((new_h, new_w, c), dtype=img.dtype)
+
+    scale_x: float = w / new_w
+    scale_y: float = h / new_h
+
+    for new_y in range(new_h):
+        for new_x in range(new_w):
+            original_x: int = min(int(new_x * scale_x), w-1)
+            original_y: int = min(int(new_y * scale_y), w-1)
+
+            resized_img[new_y, new_x] = img[original_y, original_x]
+
+    return resized_img
+
 
 def main():
     image = cv2.imread("squareimage.png")
