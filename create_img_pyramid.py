c51bace3f4eb47aeea8ffba6ff57d3e746a60601
Amogh <axm0754@mavs.uta.edu>
2025-01-29 22:59:36 -0600

    Refactor create_img_pyramid to accept an image array and return a list of resized images; add demo_image_pyramid function for visualization

--- create_img_pyramid.py
+++ create_img_pyramid.py
@@ -3,14 +3,28 @@ import numpy as np
 from img_transforms import resize_img
 import cv2
 
-def create_img_pyramid(filename: str, height: int) -> None:
-    img = cv2.imread(filename)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+def create_img_pyramid(img: np.ndarray, height: int) -> list[np.ndarray]:
+    """
+    Create an image pyramid by resizing the input image to different scales.
+
+    Parameters
+    ----------
+    img : np.ndarray
+        Input image of shape (H, W, C), where H is the height, W is the width, and C is the number of channels.
+    height : float
+        The number of scales to resize the image to.
+
+    Returns
+    -------
+    list[np.ndarray]
+        List of resized images, where each image has shape (new_H, new_W, C), with the same dtype as the input.
+    """
     h, w, c = img.shape
+    resized_imgs = []
     for i in range(height):
         resized_img = resize_img(img, 0.5**(i+1))
-        cv2.imwrite(f"{filename.split(".png")[0]}_{(i+1)*2}x.png", cv2.cvtColor(resized_img, cv2.COLOR_BGR2RGB))
-    return
+        resized_imgs.append(resized_img)
+    return resized_imgs
 
 def main(argc: int, argv: list):
     if argc != 3:
@@ -25,7 +39,12 @@ def main(argc: int, argv: list):
                         "Filename : String\n"
                         "Height : int\n")
 
-    create_img_pyramid(filename, height)
+    img = cv2.imread(filename)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    resized_imgs = create_img_pyramid(img, height)
+
+    for i in range(height):
+        cv2.imwrite(f"{filename[:-4]}_{2**(i+1)}x.png", cv2.cvtColor(resized_imgs[i], cv2.COLOR_RGB2BGR))
 
 if __name__ == "__main__":
     import sys
